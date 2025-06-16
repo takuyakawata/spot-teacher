5cf0321091f540b525ce5b65ce4659d108a2a2d5
norishio <131588958+takuyakawata@users.noreply.github.com>
2025-06-16 18:08:06 +0900

    add find companies

---
+++ admin/src/main/kotlin/com/spotteacher/admin/feature/company/usecase/FindCompaniesUseCase.kt
@@ -0,0 +1,12 @@
+package com.spotteacher.admin.feature.company.usecase
+
+import com.spotteacher.admin.feature.company.domain.Company
+import com.spotteacher.admin.feature.company.domain.CompanyRepository
+import com.spotteacher.usecase.UseCase
+
+@UseCase
+class FindCompaniesUseCase(
+    private val companyRepository: CompanyRepository
+) {
+    suspend fun call():List<Company> = companyRepository.getAll()
+}
