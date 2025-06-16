acbb48343de9ffc5c895b477f7e85a565d23c5c9
norishio <131588958+takuyakawata@users.noreply.github.com>
2025-06-17 07:03:07 +0900

    admin user entity

--- admin/src/main/kotlin/com/spotteacher/admin/feature/adminUser/domain/AdminUser.kt
+++ admin/src/main/kotlin/com/spotteacher/admin/feature/adminUser/domain/AdminUser.kt
@@ -1,15 +1,61 @@
 package com.spotteacher.admin.feature.adminUser.domain
 
+import com.spotteacher.domain.EmailAddress
 import com.spotteacher.util.Identity
 
-data class AdminUser(
-    val id: AdminUserId,
-    val firstName: AdminName,
-    val lastName: AdminName,
-    val email: Email,
+sealed interface AdminUser{
+    val id: AdminUserId
+    val firstName: AdminName
+    val lastName: AdminName
+}
+
+data class ActiveAdminUser(
+    override val id: AdminUserId,
+    override val firstName: AdminName,
+    override val lastName: AdminName,
+    val email: EmailAddress,
     val password: Password,
-    val isActive: Boolean
-)
+) : AdminUser {
+    companion object{
+        fun create(
+            firstName: AdminName,
+            lastName: AdminName,
+            email: EmailAddress,
+            password: Password,
+        ) = ActiveAdminUser(
+            id = AdminUserId(0),
+            firstName = firstName,
+            lastName = lastName,
+            email = email,
+            password = password,
+        )
+    }
+}
+
+
+/**
+ * Represents an inactive admin user.(使用停止中のユーザー）
+ *
+ * @property id The unique identifier for the admin user.
+ * @property firstName The first name of the admin user.
+ * @property lastName The last name of the admin user.
+ */
+data class InActiveAdminUser(
+    override val id: AdminUserId,
+    override val firstName: AdminName,
+    override val lastName: AdminName,
+) : AdminUser {
+    companion object{
+       fun create(
+           firstName: AdminName,
+           lastName: AdminName,
+       ) = InActiveAdminUser(
+           id = AdminUserId(0),
+           firstName = firstName,
+           lastName = lastName,
+       )
+    }
+}
 
 //Entity のIDは
 class AdminUserId(override val value: Long) : Identity<Long>(value)
@@ -22,23 +68,6 @@ value class AdminName(val value: String){
     init {
         require(value.length <= MAX_LENGTH) {
             "AdminName must be less than $MAX_LENGTH characters"
-        }
-    }
-}
-
-@JvmInline
-value class Email(val value: String){
-    companion object {
-        const val MAX_LENGTH = 500
-    }
-    init {
-        require(value.length <= MAX_LENGTH) {
-            "Email must be less than $MAX_LENGTH characters"
-        }
-
-        // メールの形式になっているか
-        require(value.matches(Regex("^[a-zA-Z0-9_!#$%&’*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$"))) {
-            "Invalid email format"
         }
     }
 }
