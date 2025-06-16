28867826099ad786b1a115bc55d9fa1a5fd6a572
norishio <131588958+takuyakawata@users.noreply.github.com>
2025-06-16 17:50:13 +0900

    add test

---
+++ admin/src/test/kotlin/com/spotteacher/admin/fixture/CompanyFixture.kt
@@ -0,0 +1,65 @@
+package com.spotteacher.admin.fixture
+
+import com.spotteacher.admin.feature.company.domain.Company
+import com.spotteacher.admin.feature.company.domain.CompanyId
+import com.spotteacher.admin.feature.company.domain.CompanyName
+import com.spotteacher.admin.feature.company.domain.CompanyRepository
+import com.spotteacher.domain.Address
+import com.spotteacher.domain.City
+import com.spotteacher.domain.PhoneNumber
+import com.spotteacher.domain.PostCode
+import com.spotteacher.domain.Prefecture
+import com.spotteacher.domain.StreetAddress
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Component
+import java.net.URI
+
+@Component
+class CompanyFixture {
+
+    @Autowired
+    private lateinit var companyRepository : CompanyRepository
+    private var companyIdCount = 1L
+
+    fun buildCompany(
+        id: CompanyId = CompanyId(companyIdCount++),
+        name: CompanyName = CompanyName("test company"),
+        address: Address = defaultAddress(),
+        phoneNumber: PhoneNumber = PhoneNumber("1234567890"),
+        url :URI = URI("https://example.com"),
+        createdAt: java.time.LocalDateTime = java.time.LocalDateTime.now()
+        ): Company {
+        return Company(
+            id = id,
+            name = name,
+            address = address,
+            phoneNumber = phoneNumber,
+            url = url,
+            createdAt = createdAt,
+        )
+    }
+
+    suspend fun createCompany(
+        name: CompanyName = CompanyName("test company"),
+        address: Address = defaultAddress(),
+        phoneNumber: PhoneNumber = PhoneNumber("12345678"),
+        url :URI = URI("https://example.com"),
+        createdAt: java.time.LocalDateTime = java.time.LocalDateTime.now()
+    ): Company {
+        val company = buildCompany(
+            name = name,
+            address = address,
+            phoneNumber = phoneNumber,
+            url = url,
+            createdAt = createdAt,
+        )
+        return companyRepository.create(company)
+    }
+}
+
+fun defaultAddress() = Address(
+        postCode = PostCode("1234567"),
+        prefecture = Prefecture.TOKYO, city = City("Tokyo"),
+        streetAddress = StreetAddress("Test Street"),
+        buildingName = null,
+    )
--- shared/src/main/kotlin/com/spotteacher/domain/PhoneNumber.kt
+++ shared/src/main/kotlin/com/spotteacher/domain/PhoneNumber.kt
@@ -3,10 +3,10 @@ package com.spotteacher.domain
 @JvmInline
 value class PhoneNumber(val value: String){
     companion object {
-        private const val MAX_LENGTH = 10
+        private const val MAX_LENGTH = 12
     }
     init {
-        require(value.length == MAX_LENGTH) {
+        require(value.length <= MAX_LENGTH) {
             "Phone number must be $MAX_LENGTH digits"
         }
     }
