90a6fbf4b03a9d3f61a67d296b9dfb77a4a93b2f
norishio <131588958+takuyakawata@users.noreply.github.com>
2025-06-16 14:26:55 +0900

    add company domain

---
+++ admin/src/main/kotlin/com/spotteacher/admin/feature/company/domain/Company.kt
@@ -0,0 +1,22 @@
+package com.spotteacher.admin.feature.company.domain
+
+import com.spotteacher.domain.Address
+import com.spotteacher.util.Identity
+import java.time.LocalDateTime
+
+data class Company(
+    val id: CompanyId,
+    val name: CompanyName,
+    val address : Address,
+    val createdAt : LocalDateTime
+)
+
+class CompanyId(override val value:Long): Identity<Long>(value)
+
+@JvmInline
+value class CompanyName(val value: String)
+
+enum class CompanyErrorCode {
+    COMPANY_NOT_FOUND,
+    COMPANY_ALREADY_EXISTS,
+}
