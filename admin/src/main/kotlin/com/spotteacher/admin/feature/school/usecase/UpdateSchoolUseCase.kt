3e3219dcf2cbcb4de419a4ff1fdf53caeda10951
norishio <131588958+takuyakawata@users.noreply.github.com>
2025-06-19 19:45:58 +0900

    fix upload files

--- admin/src/main/kotlin/com/spotteacher/admin/feature/school/usecase/UpdateSchoolUseCase.kt
+++ admin/src/main/kotlin/com/spotteacher/admin/feature/school/usecase/UpdateSchoolUseCase.kt
@@ -1,12 +1,56 @@
 package com.spotteacher.admin.feature.school.usecase
 
-//
-// @UseCase
-// class UpdateSchoolUseCase(
-//    private val repository: SchoolRepository
-// ) {
-//    suspend fun call(id: SchoolId): Either<SchoolError, Unit> {
-//        val school = repository.findById(id)
-//
-//    }
-// }
+import arrow.core.Either
+import arrow.core.left
+import arrow.core.right
+import com.spotteacher.admin.feature.school.domain.School
+import com.spotteacher.admin.feature.school.domain.SchoolCategory
+import com.spotteacher.admin.feature.school.domain.SchoolError
+import com.spotteacher.admin.feature.school.domain.SchoolErrorCode
+import com.spotteacher.admin.feature.school.domain.SchoolId
+import com.spotteacher.admin.feature.school.domain.SchoolName
+import com.spotteacher.admin.feature.school.domain.SchoolRepository
+import com.spotteacher.admin.shared.infra.TransactionCoroutine
+import com.spotteacher.domain.Address
+import com.spotteacher.domain.PhoneNumber
+import com.spotteacher.usecase.UseCase
+
+data class UpdateSchoolUseCaseInput(
+    val schoolId: SchoolId,
+    val name: SchoolName?,
+    val schoolCategory: SchoolCategory?,
+    val address: Address?,
+    val phoneNumber: PhoneNumber?,
+    val url: String?
+)
+
+data class UpdateSchoolUseCaseOutput(
+    val result: Either<SchoolError, Unit>
+)
+
+@UseCase
+class UpdateSchoolUseCase(
+    private val schoolRepository: SchoolRepository
+) {
+    @TransactionCoroutine
+    suspend fun call(input: UpdateSchoolUseCaseInput): UpdateSchoolUseCaseOutput {
+        val school = schoolRepository.findById(input.schoolId) ?: return UpdateSchoolUseCaseOutput(
+            SchoolError(
+                code = SchoolErrorCode.SCHOOL_NOT_FOUND,
+                message = "School not found"
+            ).left()
+        )
+
+        val updatedSchool = school.copy(
+            name = input.name ?: school.name,
+            schoolCategory = input.schoolCategory ?: school.schoolCategory,
+            address = input.address ?: school.address,
+            phoneNumber = input.phoneNumber ?: school.phoneNumber,
+            url = input.url ?: school.url
+        )
+
+        schoolRepository.update(updatedSchool)
+
+        return UpdateSchoolUseCaseOutput(Unit.right())
+    }
+}
---
+++ admin/src/main/kotlin/com/spotteacher/admin/feature/uploadFile/infra/UploadFileRepositoryImpl.kt
@@ -0,0 +1,115 @@
+package com.spotteacher.admin.feature.uploadFile.infra
+
+import arrow.core.Nel
+import com.spotteacher.admin.feature.uploadFile.domain.FileKey
+import com.spotteacher.admin.feature.uploadFile.domain.UploadFile
+import com.spotteacher.admin.feature.uploadFile.domain.UploadFileId
+import com.spotteacher.admin.feature.uploadFile.domain.UploadFileRepository
+import com.spotteacher.admin.feature.uploadFile.domain.UploadStatus
+import com.spotteacher.admin.shared.infra.S3Client
+import com.spotteacher.admin.shared.infra.TransactionAwareDSLContext
+import com.spotteacher.extension.nonBlockingFetch
+import com.spotteacher.infra.db.enums.UploadFilesUploadStatus
+import com.spotteacher.infra.db.tables.UploadFiles.Companion.UPLOAD_FILES
+import com.spotteacher.infra.db.tables.records.UploadFilesRecord
+import kotlinx.coroutines.reactive.awaitFirstOrNull
+import kotlinx.coroutines.reactive.awaitLast
+import org.springframework.stereotype.Repository
+
+@Repository
+class UploadFileRepositoryImpl(
+    private val dslContext: TransactionAwareDSLContext,
+    private val s3Client: S3Client,
+) : UploadFileRepository {
+    override suspend fun create(uploadFile: UploadFile): UploadFile {
+        val id = dslContext.get().insertInto(
+            UPLOAD_FILES,
+            UPLOAD_FILES.FILE_KEY,
+            UPLOAD_FILES.UPLOAD_STATUS,
+        ).values(
+            uploadFile.fileKey.value,
+            UploadFilesUploadStatus.valueOf(uploadFile.status.name)
+        ).returning(UPLOAD_FILES.ID)
+            .awaitFirstOrNull()?.id!!
+
+        return uploadFile.copy(id = UploadFileId(id))
+    }
+
+    override suspend fun bulkCreate(files: Nel<UploadFile>): List<UploadFile> {
+        val queries = files.map {
+            dslContext.get().insertInto(
+                UPLOAD_FILES,
+                UPLOAD_FILES.FILE_KEY,
+                UPLOAD_FILES.UPLOAD_STATUS,
+            ).values(
+                it.fileKey.value,
+                UploadFilesUploadStatus.valueOf(it.status.name)
+            )
+        }
+        dslContext.get().batch(queries).awaitLast()
+
+        return dslContext.get().nonBlockingFetch(
+            UPLOAD_FILES,
+            UPLOAD_FILES.FILE_KEY.`in`(files.map { it.fileKey.value }),
+        ).toList()
+            .map { it.toEntity() }
+    }
+
+    override suspend fun findById(id: UploadFileId): UploadFile? {
+        return dslContext.get().selectFrom(UPLOAD_FILES)
+            .where(UPLOAD_FILES.ID.eq(id.value))
+            .awaitFirstOrNull()
+            ?.toEntity()
+    }
+
+    override suspend fun filterByIds(ids: Nel<UploadFileId>): List<UploadFile> {
+        return dslContext.get().nonBlockingFetch(
+            UPLOAD_FILES,
+            UPLOAD_FILES.ID.`in`(ids.map { it.value })
+        ).map { it.toEntity() }
+    }
+
+    override suspend fun bulkUpdate(files: Nel<UploadFile>) {
+        val queries = files.map {
+            dslContext.get().update(UPLOAD_FILES)
+                .set(
+                    UPLOAD_FILES.UPLOAD_STATUS,
+                    UploadFilesUploadStatus.valueOf(it.status.name)
+                )
+                .where(UPLOAD_FILES.ID.eq(it.id.value))
+        }
+
+        dslContext.get().batch(queries).awaitLast()
+    }
+
+    override suspend fun generateUploadUrl(fileKey: FileKey): String {
+        return s3Client.generateUploadUrl(fileKey.value)
+    }
+
+    override suspend fun generateDownloadUrl(fileKey: FileKey): String {
+        return s3Client.generateDownloadUrl(fileKey.value)
+    }
+
+    override suspend fun bulkDelete(ids: Nel<UploadFileId>) {
+        val queries = ids.map {
+            dslContext.get().deleteFrom(UPLOAD_FILES)
+                .where(UPLOAD_FILES.ID.eq(it.value))
+        }
+
+        dslContext.get().batch(queries).awaitLast()
+    }
+
+    override suspend fun deleteFile(fileKey: FileKey) {
+        s3Client.delete(fileKey.value)
+    }
+
+    private fun UploadFilesRecord.toEntity(): UploadFile {
+        return UploadFile(
+            id = UploadFileId(id!!),
+            fileKey = FileKey(fileKey),
+            status = UploadStatus.valueOf(uploadStatus.name),
+        )
+    }
+}
+
+
